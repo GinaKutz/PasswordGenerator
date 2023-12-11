0f71091bada1da82fea5ad6f39bd64d831e3609b
unknown <gjkutz@gmail.com>
2023-12-11 07:23:02 -0600

    Code that I have been working on

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # PasswordGenerator
+
+## Description
+
+This is a randomized password generator. Having this tool in my toolbox as a piece of code that I can use by adding it in to future projects is a miniature project I am happy to have spent time working through.
+
+The greatest challenge in this project was generating the alert if the password size was not within the correct range and in specifying each type of character to be used.
+
+
+## Credits
+
+ I primarily worked on this alone, however I worked with Travis Umbel, Keely York, Megan Schneider and Cameron Kirk doing the pseudo code. I relied heavily on Google for information.
--- script.js
+++ script.js
@@ -1,5 +1,53 @@
 // Assignment code here
 
+function generatePassword() {
+  const lowercaseChars = 'abcdefghijklmnopqrstuvwxyz';
+  const uppercaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const numericChars = '0123456789';
+  const specialChars = '!@#$%^&*()-_+=<>?{}[]|';
+
+  let allChars = '';
+  let password = '';
+
+  const length = prompt("Enter the length of the password (between 8 and 128)");
+
+  if (length < 8 || length > 128 || isNaN(length)) {
+    alert("Please enter a valid password length between 8 and 128.");
+    return;
+  }
+
+  const includeLowercase = confirm("Include lowercase characters?");
+  const includeUppercase = confirm("Include uppercase characters?");
+  const includeNumeric = confirm("Include numeric characters?");
+  const includeSpecial = confirm("Include special characters?");
+
+  if (!(includeLowercase || includeUppercase || includeNumeric || includeSpecial)) {
+    alert("At least one character type must be selected.");
+    return;
+  }
+
+  if (includeLowercase) {
+    allChars += lowercaseChars;
+  }
+  if (includeUppercase) {
+    allChars += uppercaseChars;
+  }
+  if (includeNumeric) {
+    allChars += numericChars;
+  }
+  if (includeSpecial) {
+    allChars += specialChars;
+  }
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * allChars.length);
+    password += allChars[randomIndex];
+  }
+
+  return password;
+}
+
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -13,5 +61,12 @@ function writePassword() {
 
 }
 
+// Call the function to generate the password based on user input
+const newPassword = generatePassword();
+
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Display the generated password
+alert("Your generated password is: " + newPassword);
